67c4559a1fb4b8ba3ec8c2978b6a36362ab7f475
kanchihari <harishkanchi.rishi@gmail.com>
2023-08-25 21:41:10 +0200

    dispaly addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,71 @@
-//Wait for the DOM to finish loading before running the game
-// Get the button elemrnts and add event listeners to them
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
-    //here in the for loop we are using for(let i=0;i<buttons.length;i++)same expression in different way
-    for(let button of buttons){
-        button.addEventListener("click",function(){
-            if(this.getAttribute("data-type") ==="submit"){
-                alert("You clicked submit!");
-            }else{
-                let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);//using the dollar sign and curly braces to reffer to the game type
-            }
-        })
 
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
     }
-})
+
+    runGame("addition");
+
+});
 
 /**
- * The main game"loop",called when the script is first loaded
- * and after the user's answer has processed
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
  */
-function rungame(){
-    //Creates two random numbers between 1 and 25
-    let num1=Math.floor(Math.random() * 25)+1;
-    let num2=Math.floor(Math.random() * 25)+1;
-}
-function chechAnswer(){
+function runGame(gameType) {
+
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
-function calculateCorrectAnswer(){
+
+function checkAnswer() {
 
 }
-function incorrectScore(){
+
+function calculateCorrectAnswer() {
 
 }
-function incrementWorngAnswer(){
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
     
 }
-function displayAdditionQuestion(){
+
+function displaySubtractQuestion() {
 
 }
-function displaySubtractQuestion(){
 
-}
-function displayMultiplyQuestion(){
-
+function displayMultiplyQuestion() {
+    
 }
